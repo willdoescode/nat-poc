75020ca87140323b6b5ee5e4aeadb89d2958a30e
Will Lane <williamlane923@gmail.com>
2020-11-18 11:27:02 -0800

    added file searching

--- src/input.rs
+++ src/input.rs
@@ -3,6 +3,19 @@ use structopt::StructOpt;
 /// The ls replacement you never knew you needed
 #[derive(StructOpt)]
 pub struct Cli {
+  /// Give me a directory
   #[structopt(parse(from_os_str), default_value=".")]
   pub dir: std::path::PathBuf,
+
+  #[structopt(short = "n", long = "name")]
+  pub name: bool,
+
+  #[structopt(short = "c", long = "created")]
+  pub created: bool,
+
+  #[structopt(short = "m", long = "modified")]
+  pub modified: bool,
+
+  #[structopt(short = "s", long = "size")]
+  pub size: bool,
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use chrono;
 use filetime;
 use std::os::unix::fs::{MetadataExt, PermissionsExt};
 use structopt::StructOpt;
+use std::fmt;
 
 #[derive(Debug)]
 struct Directory {
@@ -42,18 +43,43 @@ fn get_sort_type(sort_t: [bool; 4]) -> DirSortType {
 
 impl Directory {
   fn new(dir: std::path::PathBuf) -> Result<Self, Box<dyn std::error::Error>> {
-    let paths = std::fs::read_dir(dir)?
-      .map(|res| res.map(|e| e.path()))
-      .collect::<Result<Vec<std::path::PathBuf>, std::io::Error>>()?;
-    Ok(
-      Self {
-        paths
+    if !std::path::Path::new(&dir).exists() {
+      let mut new_paths = Vec::new();
+      let paths = std::fs::read_dir(".")?
+        .map(|res| res.map(|e| e.path()))
+        .collect::<Result<Vec<std::path::PathBuf>, std::io::Error>>()?;
+      for p in paths {
+        if p
+          .file_name()
+            .unwrap()
+            .to_str()
+            .unwrap()
+            .to_lowercase()
+            .contains(&dir.display().to_string().to_lowercase())
+            {
+              new_paths.push(p)
+            }
       }
-    )
+      Ok (
+        Self {
+          paths: new_paths
+        }
+      )
+    }
+    else {
+      let paths = std::fs::read_dir(dir)?
+        .map(|res| res.map(|e| e.path()))
+        .collect::<Result<Vec<std::path::PathBuf>, std::io::Error>>()?;
+      Ok(
+        Self {
+          paths
+        }
+      )
+    }
   }
 
   fn sort_paths(&mut self) {
-    match get_sort_type([false, false, true, false]) {
+    match get_sort_type([input::Cli::from_args().name, input::Cli::from_args().created, input::Cli::from_args().modified, input::Cli::from_args().size]) {
       DirSortType::Name => { 
         self.paths.sort_by(|a, b| a.file_name().unwrap().to_str().unwrap().to_lowercase().cmp(&b.file_name().unwrap().to_str().unwrap().to_lowercase()))
       },
@@ -64,17 +90,23 @@ impl Directory {
         self.paths.sort_by(|a, b| a.symlink_metadata().unwrap().modified().unwrap().cmp(&b.symlink_metadata().unwrap().modified().unwrap()))
       },
       DirSortType::Size => { 
-        self.paths.sort_by(|a, b| a.symlink_metadata().unwrap().size().cmp(&b.symlink_metadata().unwrap().size()) )
+        self.paths.sort_by(|a, b| a.symlink_metadata().unwrap().size().cmp(&b.symlink_metadata().unwrap().size()))
       },
-      DirSortType::Not => { 
-        self.paths.sort_by(|a, b| a.file_name().unwrap().to_str().unwrap().to_lowercase().cmp(&b.file_name().unwrap().to_str().unwrap().to_lowercase()))
-      },
+      DirSortType::Not => (),
     }
+  }
+}
+
+
+impl fmt::Display for Directory {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    let vec = &self.paths;
+    write!(f, "{:?}", vec)
   }
 }
 
 fn main() {
   let mut dir = Directory::new(input::Cli::from_args().dir).unwrap();
   dir.sort_paths();
-  println!("{:?}", dir)
+  println!("{}", dir)
 }
